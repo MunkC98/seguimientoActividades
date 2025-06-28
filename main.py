fc9cef5e1eccc5d57ed6133af0fcd14ad70684c1
FroggyBoy <diegoprado1618@outlook.com>
2025-06-27 18:28:37 -0600

    Se añadio el header y se añadio la carpeta que contendra los datos

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import pandas as pd
 import streamlit as st
-import  os
-import 
+import datetime as dt
+import os
+
+hoy = dt.date.today()
+nombre_dia = hoy.strftime('%A')
+path_data = 'data/data.csv'
+
+st.title("Seguimiento de las actividades - " + f'{hoy}')
+
+st.header(nombre_dia)
+
+df = pd.read_csv(path_data)
+
+st.write(df)
